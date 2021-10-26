837ca17df2de3405463d59bf73273fc1d2b96b51
26973 <shuai_li_4@sfu.ca>
2021-10-26 02:54:51 -0700

    Main screen design complete
    Next thing is writing code in MainActivity correlate with button

--- app/src/main/java/com/example/cmpt276_2021_7_manganese/MainActivity.java
+++ app/src/main/java/com/example/cmpt276_2021_7_manganese/MainActivity.java
@@ -10,6 +10,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.view.View;
 
+import androidx.appcompat.widget.Toolbar;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -19,10 +20,13 @@ import com.example.cmpt276_2021_7_manganese.databinding.ActivityMainBinding;
 
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    private Button btnFlipCoin;
+    private Button btnTimer;
+    private Button btnChildManager;
 
 
 
@@ -30,6 +34,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        //////////////////////////////////////////////////////////////////////////////////////////
+        this.setTitle("Main Menu");
+
+
 
 
         FloatingActionButton fab = findViewById(R.id.floatingActionButton);
