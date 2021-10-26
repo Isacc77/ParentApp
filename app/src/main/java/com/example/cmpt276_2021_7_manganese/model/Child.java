68df1e25ad3a8ee9bb18b59c86633553a86975cd
26973 <shuai_li_4@sfu.ca>
2021-10-26 01:50:34 -0700

    Build the child and ChildManager class

--- app/src/main/java/com/example/cmpt276_2021_7_manganese/model/Child.java
+++ app/src/main/java/com/example/cmpt276_2021_7_manganese/model/Child.java
@@ -1,4 +1,34 @@
 package com.example.cmpt276_2021_7_manganese.model;
 
+/**
+ * This class is for child
+ *
+ *
+ */
+
+
 public class Child {
+
+    String name;
+
+    public Child(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Child{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+
 }
--- app/src/main/java/com/example/cmpt276_2021_7_manganese/model/ChildManager.java
+++ app/src/main/java/com/example/cmpt276_2021_7_manganese/model/ChildManager.java
@@ -1,4 +1,75 @@
 package com.example.cmpt276_2021_7_manganese.model;
 
-public class ChildManager {
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class ChildManager implements Iterable<Child> {
+
+    private ArrayList<Child> manager = new ArrayList<>();
+
+    private static ChildManager instance;
+
+
+    public ChildManager() {
+    }
+
+
+    public static ChildManager getInstance() {
+
+        if (instance == null) {
+
+            instance = new ChildManager();
+
+        }
+
+        return instance;
+    }
+
+
+    public void add(Child child) {
+        manager.add(child);
+    }
+
+    public void removeChild(int index) {
+        if (index < 0 || index > manager.size()) {
+
+            throw new IndexOutOfBoundsException("Index out of range");
+
+        } else {
+
+            manager.remove(index);
+
+        }
+
+    }
+
+
+    public int getSize() {
+        return manager.size();
+    }
+
+
+    public ArrayList<Child> getManager() {
+        return manager;
+    }
+
+    public void setManager(ArrayList<Child> manager) {
+        this.manager = manager;
+    }
+
+
+    @Override
+    public Iterator<Child> iterator() {
+        return manager.iterator();
+    }
+
+
+    public void printAll() {
+        int cnt = 0;
+        for(Child c : manager) {
+            System.out.println(cnt++ + ": " + c);
+        }
+    }
+
+
 }
