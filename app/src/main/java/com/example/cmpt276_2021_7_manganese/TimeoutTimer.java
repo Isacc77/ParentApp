c98f6c46d51659f9ca0ec56497b36bd22868c916
Rio Samson <rbs5@sfu.ca>
2021-10-31 01:47:32 -0700

    Made the radio button select a default value and save the data using shared preferences. Saved radio button selected data during app runs. @Rio Samson

--- app/src/main/java/com/example/cmpt276_2021_7_manganese/TimeoutTimer.java
+++ app/src/main/java/com/example/cmpt276_2021_7_manganese/TimeoutTimer.java
@@ -1,10 +1,15 @@
+
 package com.example.cmpt276_2021_7_manganese;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 /**
  * This TimeoutTimer activity represents the screen containing timeout features.
@@ -12,11 +17,50 @@ import android.os.Bundle;
  * @author Rio Samson
  */
 public class TimeoutTimer extends AppCompatActivity {
+    static private final String PREFS_TAG = "Time Settings";
+    static private final String SAVE_TIMER_KEY = "Different timer settings";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_timeout_timer);
+        setupTimerSettings();
+    }
+
+    private int loadSavedData() {
+        SharedPreferences prefs = this.getSharedPreferences(PREFS_TAG,
+                MODE_PRIVATE);
+        return prefs.getInt(SAVE_TIMER_KEY, getResources().getInteger(R.integer.default_timer));
+    }
+
+    private void setupTimerSettings() {
+        RadioGroup radioGroup = findViewById(R.id.timeRadioGroup);
+        int[] timerSettings = getResources().getIntArray(R.array.times);
+
+        for(int i = 0; i < timerSettings.length; i++) {
+            final int setting = timerSettings[i];
+            RadioButton button = new RadioButton(this);
+            button.setText("" + setting);
+
+            button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    saveTimeSettings(setting);
+                }
+            });
+            radioGroup.addView(button);
+            int savedSetting = loadSavedData();
+            if (setting == savedSetting) {
+                button.setChecked(true);
+            }
+        }
+    }
+
+    private void saveTimeSettings(int timerSettings) {
+        SharedPreferences prefs = this.getSharedPreferences(PREFS_TAG, MODE_PRIVATE);
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putInt(SAVE_TIMER_KEY, timerSettings);
+        editor.apply();
     }
 
     public static Intent makeLaunchIntent(Context c) {
