a255362beb5ebeb6381bf8d756cf3bedc63e5d02
26973 <shuai_li_4@sfu.ca>
2021-11-08 01:29:13 -0800

    try to merge

--- app/src/main/java/com/example/cmpt276_2021_7_manganese/MainActivity.java
+++ app/src/main/java/com/example/cmpt276_2021_7_manganese/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.cmpt276_2021_7_manganese;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 
