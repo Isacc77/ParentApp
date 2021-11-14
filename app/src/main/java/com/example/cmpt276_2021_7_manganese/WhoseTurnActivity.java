28e46851ca89d99ce06ee1b33fc3e37337cdf4a4
26973 <shuai_li_4@sfu.ca>
2021-11-14 02:41:17 -0800

    Basic structure for WhoseTurnActivity complete

--- app/src/main/java/com/example/cmpt276_2021_7_manganese/WhoseTurnActivity.java
+++ app/src/main/java/com/example/cmpt276_2021_7_manganese/WhoseTurnActivity.java
@@ -1,29 +1,56 @@
 package com.example.cmpt276_2021_7_manganese;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.example.cmpt276_2021_7_manganese.model.TaskManager;
 
 public class WhoseTurnActivity extends AppCompatActivity {
 
-    public static Intent makeLaunchIntent(Context c) {
-        return new Intent(c, WhoseTurnActivity.class);
-    }
+    private TaskManager t_manager;
+
+    private TextView tv_notice;
+    private TextView emptyListInfo;
+    private ListView lv_task_data;
+    private Toolbar toolbar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_whose_turn);
+        tv_notice = findViewById(R.id.tv_hint_for_adding_task);
+        lv_task_data = findViewById(R.id.lv_manage_task);
+        toolbar = findViewById(R.id.tb_manage_task);
+
+        setUpToolBar(toolbar);
+        tv_notice.setSelected(true);
+
+
     }
 
 
 
 
 
+    private void setUpToolBar(Toolbar toolbar) {
+        setSupportActionBar(toolbar);
+        ActionBar ab = getSupportActionBar();
+        ab.setDisplayHomeAsUpEnabled(true);
+    }
 
 
+
+    public static Intent makeLaunchIntent(Context c) {
+        return new Intent(c, WhoseTurnActivity.class);
+    }
 
 
 
